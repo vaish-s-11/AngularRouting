ad91655362ff5bbc8acd8e7964771bdedc520c77
vaish-s-11 <132575819+vaish-s-11@users.noreply.github.com>
2023-05-17 18:07:57 +0530

    Add files via upload

---
+++ Characters/Characters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Characters',
+  templateUrl: './Characters.component.html',
+  styleUrls: ['./Characters.component.css']
+})
+export class CharactersComponent {
+
+}
